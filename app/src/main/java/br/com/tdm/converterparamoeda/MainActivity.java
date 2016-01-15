807363de23c4ce34e509ccda03833f9b66337e7f
Manuel Ferreira da Costa Filho <mnferr2004@yahoo.com.br>
2016-01-15 13:48:16 -0300

    último commit versão 1.0
    mudança do ícone

--- app/src/main/java/br/com/tdm/converterparamoeda/MainActivity.java
+++ app/src/main/java/br/com/tdm/converterparamoeda/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.tdm.converterparamoeda;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -10,23 +11,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    Button calcular;
+    ImageButton calcular;
+    ImageButton btn_editMoeda;
+    ImageButton btn_editCotacao;
 
     EditText edt_cotacao;
     EditText edt_qtde;
     TextView view_reais;
+    EditText edt_moeda;
+
 
     SharedPreferences arqDeDadosDaCotacao;
-
 
     private float cotacao;
     private float valorMoeda;
     private float reais;
+    private String moeda;
 
     final static String NOME_FILE = "convertermoeda";
 
@@ -37,21 +46,59 @@ public class MainActivity extends ActionBarActivity {
 
         arqDeDadosDaCotacao = getSharedPreferences(NOME_FILE, MODE_PRIVATE);
 
-        cotacao = arqDeDadosDaCotacao.getFloat("cotacao",0);
-
-        if (cotacao != 0) {
-
+        cotacao =  arqDeDadosDaCotacao.getFloat("cotacao",0) ;
+        if (cotacao > 0) {
             edt_cotacao = (EditText) findViewById(R.id.edt_cotacao);
-
             edt_cotacao.setText(cotacao+"");
+        }
 
-
+        moeda = arqDeDadosDaCotacao.getString("moeda","");
+        if (!moeda.equals("") ) {
+            edt_moeda = (EditText) findViewById(R.id.edt_moeda);
+            edt_moeda.setText(moeda);
         }
 
 
+        // setando o focus
+        edt_qtde = (EditText) findViewById(R.id.edt_qtde);
+        edt_qtde.setText("   ");
+        edt_qtde.findFocus();
+        edt_qtde.setFocusable(true);
 
-        calcular = (Button) findViewById(R.id.btn_calcular);
+        // botão edit moeda
+        btn_editMoeda = (ImageButton) findViewById(R.id.btn_editMoeda);
+        btn_editMoeda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            edt_moeda = (EditText) findViewById(R.id.edt_moeda);
+            edt_moeda.setText("    ");
+            edt_moeda.setEnabled(true);
+            edt_moeda.findFocus();
+            edt_moeda.setFocusable(true);
+            mensagem("Informe a nova moeda");
+
+            }
+        });
+
+        // botão edit moeda
+        btn_editCotacao = (ImageButton) findViewById(R.id.btn_editCotacao);
+        btn_editCotacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            edt_cotacao = (EditText) findViewById(R.id.edt_cotacao);
+            edt_cotacao.setText("    ");
+            edt_cotacao.setEnabled(true);
+            edt_cotacao.findFocus();
+            edt_cotacao.setFocusable(true);
+            mensagem("Informe a nova cotação");
+
+            }
+        });
+
+
+        calcular = (ImageButton) findViewById(R.id.btn_calcular);
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,21 +117,64 @@ public class MainActivity extends ActionBarActivity {
         edt_cotacao = (EditText) findViewById(R.id.edt_cotacao);
         edt_qtde =  (EditText) findViewById(R.id.edt_qtde);
         view_reais = (TextView) findViewById(R.id.txfld_valorreais);
+        edt_moeda = (EditText) findViewById(R.id.edt_moeda);
 
+        if (edt_cotacao.getText().toString().trim().equals("")) {
+            cotacao = 0;
+            mensagem("Faltou informar o valor da cotação");
+        }else{
+            cotacao = Float.parseFloat( edt_cotacao.getText().toString());
+        }
 
-        cotacao = Float.parseFloat( edt_cotacao.getText().toString());
-        valorMoeda = Float.parseFloat(edt_qtde.getText().toString());
+        if (edt_qtde.getText().toString().trim().equals("")) {
+            valorMoeda = 0;
+            mensagem("Faltou informar o valor na moeda origem");
+        }else{
+            valorMoeda = Float.parseFloat(edt_qtde.getText().toString());
+        }
 
         reais  = valorMoeda * cotacao;
 
-        view_reais.setText(reais+"");
+        view_reais.setText( "R$ "+String.valueOf(round(reais,2) ));
 
+
+        if (edt_moeda.getText().toString().trim().equals("")) {
+            mensagem("Faltou informar o nome da moeda");
+        }else{
+            moeda = edt_moeda.getText().toString();
+        }
+
+
+        // salvando dados
         Editor editor = arqDeDadosDaCotacao.edit();
 
-        editor.putFloat("cotacao",cotacao);
+        if ( cotacao>0.00) {
+            editor.putFloat("cotacao", cotacao);
+        }
+        if (moeda != null ) {
+            editor.putString("moeda", moeda);
+        }
         editor.commit();
 
     }
+
+    private BigDecimal round( Float reais, Integer casasDecimais) {
+        BigDecimal big = new BigDecimal(reais);
+        big = big.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        return big;
+    }
+
+    private void mensagem(String mensagem){
+
+        Context contexto = getApplicationContext();
+        int duracao = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(contexto, mensagem,duracao);
+        toast.show();
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
